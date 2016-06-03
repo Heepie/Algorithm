c8eeab300b102ca90be1264666c2e701c0d9fe04
Heepie <engineer_mun@naver.com>
2016-06-03 14:37:40 +0900

    Study For Algorithm
    [2016.06.03]

--- Codility/TapeEquilibrium.cpp
+++ Codility/TapeEquilibrium.cpp
@@ -6,3 +6,31 @@
 // Problem: https://codility.com/programmers/task/tape_equilibrium/
 
 // ...Let's think of the Problem as 1 tape NOT 2 tape
+
+int getMin(int parm1, int parm2) {
+	return parm1 < parm2 ? parm1 : parm2;
+}
+
+int main () {
+	int arr[] = {3, 1};
+	printf ("%d", solution(arr, (sizeof(arr)/sizeof(int))));
+}
+
+int solution(int A[], int N) {
+	int left=0, right=0, i, min = 987654321;
+
+//	for (i=0; i<N; i++)
+//		printf ("%d\n", A[i]);
+	for (i=0; i<N; i++)
+		right += A[i];
+
+	for (i=0; i<N-1; i++) {
+		left += A[i];
+		right -= A[i];
+
+//		printf ("%d\n", abs(right- left));
+		min = getMin (min, abs(right- left));
+
+	}
+	return min;
+}
