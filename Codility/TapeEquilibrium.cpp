01018772209f1d792c4b811f1dc1c7f0081fb815
Heepie <engineer_mun@naver.com>
2016-06-03 11:53:36 +0900

    Study For Algorithm
    [2016.06.03] Codility

---
+++ Codility/TapeEquilibrium.cpp
@@ -0,0 +1,8 @@
+//============================================================================
+// Name: TapeEquilibrium.cpp
+// Author: Heepie
+//============================================================================
+
+// Problem: https://codility.com/programmers/task/tape_equilibrium/
+
+// ...Let's think of the Problem as 1 tape NOT 2 tape
