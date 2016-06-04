acab6e1ef700694a326ffe8f3636b83c819e21de
Heepie <engineer_mun@naver.com>
2016-06-04 22:37:32 +0900

    Study For Algorithm
    [2016.06.03]

---
+++ Codility/OddOccurrencesInArray.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name: BinaryGap.cpp
+// Author: Heepie
+//============================================================================
+
+// Problem: https://codility.com/programmers/task/odd_occurrences_in_array/
+
+#include <stdio.h>
+int solution(int A[], int N);
+
+int main () {
+	int arr[5] = {1,2,3,2,1};
+	printf ("%d", solution(arr, sizeof(arr)));
+}
+
+int solution(int A[], int N) {
+	int ret=0;
+	for (int i=0; i<N; i++)
+		ret ^= A[i];
+	return ret;
+}
