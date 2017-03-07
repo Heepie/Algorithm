a9a4329196b70210ec9dede8072a3749ed26d321
Heepie <Heepie@users.noreply.github.com>
2017-03-07 23:04:25 +0900

    Update OddOccurrencesInArray.java

--- Codility/OddOccurrencesInArray.java
+++ Codility/OddOccurrencesInArray.java
@@ -7,26 +7,18 @@
 
 package org.heepie.codility;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 public class OddOccurrencesInArray {
 	public static void main(String[] args) {
 		System.out.println(solution(new int[]{3,4,5,6,7,6,5,4,3}));
 	}
 
 	public static int solution (int[] A) {
-		Map<Integer, Boolean> m = new HashMap<Integer, Boolean>();
-		for (int e: A) {
-			if (!m.containsKey(e))
-				m.put(e, true);
-			else
-				m.remove(e);
-		}
-
-		Set<Integer> n = m.keySet();
-		Object[] k = n.toArray();
-		return (int)k[0];
+                int result = A[0];
+                
+                for (int i=1; i<A.length; i++) {
+                        result ^= A[i];
+                }
+                
+                return result;
 	}
 }
